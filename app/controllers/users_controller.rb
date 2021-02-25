97d48e1e2591aac59ca8b850d4329adea633711e
JohannesHupfeld <j.hupfeld@gmail.com>
2021-02-24 23:57:28 -0500

    fill out get and post login routes also created user show page route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,21 @@ class UsersController < ApplicationController
 
   # Creates session. Recieves the login form, find the user, and log user in. 
   post '/login' do
-    
+    @user = User.find_by(email: params[:email ]) # Find user
+    if @user.authenticate(params[:password]) # Checks if they have credentials - email/password combo
+      session[:user_id] = @user.id # Log user in - create the user session # Assigning key value pair to the session hash
+      puts session
+      redirect "users/#{@user.id}" # Redirect to users show page 
+    else
+
+    end
   end
 
   get '/signup' do
   end
 
+  # User SHOW page
+  get '/users/:id' do 
+    "this will be the user show route"
+  end
 end
