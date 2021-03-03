2e95031efeb01755ba0c781df482ebeeff73cb32
JohannesHupfeld <j.hupfeld@gmail.com>
2021-03-03 16:00:38 -0500

    created post recipes route --> create new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,8 +6,17 @@ class RecipesController < ApplicationController
     end
 
     # Create new recipe
-    post '/recipes' do
-    
+    post '/recipes' do # Create new recipe and save it to the DB
+      if !logged_in?
+        redirect '/'
+      end
+      if params[:name] && params[:ingredients] && params[:instructions] != ""
+        @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], instructions: params[:instructions], user_id: current_user.id) 
+        redirect "/recipes/#{@recipe.id}"
+      else
+        redirect '/recipes/new'
+      end
     end
 
+    
 end
