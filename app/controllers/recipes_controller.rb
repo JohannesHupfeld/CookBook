cdf85e68dabf2fe125ee039b978a91658248fad0
JohannesHupfeld <j.hupfeld@gmail.com>
2021-03-03 13:40:33 -0500

    added recipes/new get rout and post recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,13 @@
 class RecipesController < ApplicationController
+    
+    # Renders a form to create a new recipe
+    get '/recipes/new' do
+      erb :'/recipes/new'
+    end
+
+    # Create new recipe
+    post '/recipes' do
+    
+    end
 
 end
