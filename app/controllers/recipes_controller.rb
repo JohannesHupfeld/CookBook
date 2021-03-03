8df49292285140c6414784f5f0cf87e180265fb5
JohannesHupfeld <j.hupfeld@gmail.com>
2021-03-03 16:54:01 -0500

    started edit route for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,28 @@
-class RecipesController < ApplicationController
-    
-    # Renders a form to create a new recipe
-    get '/recipes/new' do
-      erb :'/recipes/new'
-    end
+class RecipesController < ApplicationController  
+  # Renders a form to create a new recipe
+  get '/recipes/new' do
+    erb :'/recipes/new'
+  end
 
-    # Create new recipe
-    post '/recipes' do # Create new recipe and save it to the DB
-      if !logged_in?
-        redirect '/'
-      end
-      if params[:name] && params[:ingredients] && params[:instructions] != ""
-        @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], instructions: params[:instructions], user_id: current_user.id) 
-        redirect "/recipes/#{@recipe.id}"
-      else
-        redirect '/recipes/new'
-      end
+  # Create new recipe
+  post '/recipes' do # Create new recipe and save it to the DB
+    if !logged_in?
+      redirect '/'
     end
+    if params[:name] && params[:ingredients] && params[:instructions] != ""
+      @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], instructions: params[:instructions], user_id: current_user.id) 
+      redirect "/recipes/#{@recipe.id}"
+    else
+      redirect '/recipes/new'
+    end
+  end
 
-    
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :'/recipes/show'
+  end
+
+  get '/recipes/recipes/:id/edit' do
+    erb :'recipes/edit'
+  end
 end
