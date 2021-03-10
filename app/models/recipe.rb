921519757d2c218024a3b14fa5f3c741573da826
JohannesHupfeld <j.hupfeld@gmail.com>
2021-03-10 14:35:21 -0500

    created helper method for updated_at to make date and time more readable

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,9 @@ class Recipe < ActiveRecord::Base
   def formatted_created_at
     self.created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
+
+  def formatted_updated_at
+    self.updated_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
 end
 
